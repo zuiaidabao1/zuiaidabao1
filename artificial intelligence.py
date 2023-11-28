fa79785919d7b077701119d73d2e614ed17e5c7b
zuiaidabao1 <143578581+zuiaidabao1@users.noreply.github.com>
2023-11-28 23:13:10 +0800

    Update artificial intelligence.py

--- artificial intelligence.py
+++ artificial intelligence.py
@@ -13,3 +13,22 @@ for i in range(10):
    list_1 = np.array(np.arange(1,10000))
    list_1 = np.sin(list_1)
 print("使用Numpy用时{}s".format(time.time()-start))
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('h89817032p0.png')
+kernel = np.ones((5,5),np.float32)/25
+dst = cv.filter2D(img,-1,kernel)
+blur_1 = cv.GaussianBlur(img,(5,5),0)
+blur_2 = cv.bilateralFilter(img,9,75,75)
+plt.figure(figsize=(10,10))
+plt.subplot(221),plt.imshow(img[:,:,::-1]),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(222),plt.imshow(dst[:,:,::-1]),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.subplot(223),plt.imshow(blur_1[:,:,::-1]),plt.title('Gaussian')
+plt.xticks([]), plt.yticks([])
+plt.subplot(224),plt.imshow(blur_1[:,:,::-1]),plt.title('Bilateral')
+plt.xticks([]), plt.yticks([])
+plt.show()
