ddf60e9ea509c46b7a98b393300d698a9514201c
zuiaidabao1 <143578581+zuiaidabao1@users.noreply.github.com>
2023-11-28 23:12:07 +0800

    Update artificial intelligence.py

--- artificial intelligence.py
+++ artificial intelligence.py
@@ -1,3 +1,15 @@
-import  pandas
-import  numpy
-import  math
+import numpy as np
+import math
+import random
+import time
+start = time.time()
+for i in range(10):
+   list_1 = list(range(1,10000))
+   for j in range(len(list_1)):
+       list_1[j] = math.sin(list_1[j])
+print("使用纯Python用时{}s".format(time.time()-start))
+start = time.time()
+for i in range(10):
+   list_1 = np.array(np.arange(1,10000))
+   list_1 = np.sin(list_1)
+print("使用Numpy用时{}s".format(time.time()-start))
